037791cadc5dea0904956c72af49fc5faeaf99e0
Nayeli Jordan <[nayeli@queonda.com.mx]>
2018-11-30 17:54:06 -0600

    controladores y gunciones video inicial

--- themes/irma/index.php
+++ themes/irma/index.php
@@ -11,21 +11,30 @@
             while ( $loop->have_posts() ) : $loop->the_post();
             	$custom_fields 	= get_post_custom();
 				$post_id 		= get_the_ID();
-				$videoLink 		= get_post_meta( $post_id, 'gi_video_videoLink', true );?>
+				$videoLink 		= get_post_meta( $post_id, 'gi_video_videoLink', true ); ?>
 
-				<video id="video-<?php echo $post_id; ?>" src="<?php echo $videoLink; ?>" class="width-100p" muted autoplay></video>
-	     		<div id="portada-<?php echo $post_id; ?>" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)" class="bg-image bg-absolute hide"></div>
-	     		<div id="videoControles">
+				<video id="videoHome" src="<?php echo $videoLink; ?>" class="width-100p" muted autoplay></video>
+	     		<div id="videoPortada" style="background-image: url(<?php the_post_thumbnail_url('full'); ?>)" class="bg-image bg-absolute">
+	     			<div id="buttonPlay" class="content-center"><i class="icon-play"></i></div>		     		
+	     		</div>
+	     		<div id="videoControles">	     			
+		     		<i id="videoPause" class="icon-pause"></i>
+		     		<i id="videoPlay" class="icon-play hide"></i>
 		     		<i id="videoStop" class="icon-stop"></i>
-		     		<i id="videoPlay" class="icon-play"></i>
 		     		<i id="videoVolumen" class="icon-volume"></i>
-		     		<i id="videoMute" class="icon-volume-off"></i>	     			
+		     		<i id="videoMute" class="icon-volume-off hide"></i>	     			
 	     		</div>
 
 			<?php endwhile;
 		} 
 		wp_reset_postdata(); ?>
 	</section>
+	<!-- <svg width="300" height="200">
+		<polyline points="0,100 50,0 250,0 300,100 250,200 50,200 0,100" fill="red" stroke="purple" stroke-width="0">
+		<stop offset="0" stop-color="red"/>
+            <stop offset="1" stop-color="blue"/>
+            </polyline>
+	</svg> -->
 	<section id="intro-que-hacemos" class="bg-degrade-purple_to_pink">
 		<div class="[ container ]">
 			<div class="row padding-top-bottom-slarge text-center color-light uppercase">
--- themes/irma/js/functions.js
+++ themes/irma/js/functions.js
@@ -10,6 +10,12 @@ var $=jQuery.noConflict();
 
 		$(document).ready(function() {
 			footerBottom();
+
+			/* VIDEO */
+			if ($("#videoHome")[0]){
+				console.log('Hay video');
+				runVideo();
+			}
 		});
  
 		$(window).on('resize', function(){
@@ -31,6 +37,40 @@ var $=jQuery.noConflict();
 			$('body').removeClass('overflow-hide');
 		});
 
+		/* VIDEO */
+		if ($("#videoHome")[0]){
+			$('#videoPlay, #buttonPlay').on('click', function(e){
+				$('#videoPlay, #videoPortada').addClass('hide');
+				$('#videoControles, #videoPause').removeClass('hide');
+				document.getElementById('videoHome').play();
+			});
+			$('#videoStop').on('click', function(e){
+				$('#videoControles').addClass('hide');				
+				$('#videoPortada').removeClass('hide');
+				document.getElementById('videoHome').pause();
+				document.getElementById('videoHome').currentTime = 0;
+			});
+			$('#videoPause').on('click', function(e){
+				$('#videoPause').addClass('hide');
+				$('#videoPlay').removeClass('hide');
+				document.getElementById('videoHome').pause();
+			});
+
+			/* Quitar audio */
+			$('#videoMute').on('click', function(e){
+				$(this).addClass('hide');
+				$('#videoVolumen').removeClass('hide');
+				$('video').prop('muted', true);
+			});
+			/* Activar audio */
+			//unmuteButton.addEventListener('click', function() {
+			$('#videoVolumen').on('click', function() {
+				$(this).addClass('hide');
+				$('#videoMute').removeClass('hide');
+				videoHome.muted = false;
+			});
+		}
+
 	});
 })(jQuery);
  
@@ -51,3 +91,24 @@ function getHeaderHeight(){
 function getFooterHeight(){
 	return $('footer').outerHeight();
 }// getFooterHeight
+
+
+/* VIDEO */
+function runVideo(){
+	var promise = document.getElementById('videoHome').play();
+	if (promise !== undefined) {
+		promise.then(_ => {
+			// Autoplay started!
+			$('#videoPortada').addClass('hide');
+		}).catch(error => {
+			// Autoplay was prevented.
+			$('#videoControles').addClass('hide');
+			$('#videoPortada').removeClass('hide');
+		});
+	}	
+	//Al terminar el video
+	$('#videoHome').on('ended',function(){
+		$('#videoControles').addClass('hide');
+		$('#videoPortada').removeClass('hide');
+	});
+}
