e6972d9fee0044f88191695439bf72ec9889708d
Nayeli Jordan <[nayeli@queonda.com.mx]>
2019-02-08 15:59:23 -0600

    quienes somos

---
+++ themes/irma/templates/pages-include/quienes-somos/historia.php
@@ -0,0 +1,73 @@
+<?php $lang = isset( $_GET['lang'] ) ? $_GET['lang'] : 'es'; /* Iniciar WPML */ ?>
+<div id="content-lineaTiempo" class="margin-bottom">
+    <img src="<?php echo THEMEPATH; ?>images/historia.png">
+    <div class="content-urlTiempo">
+        <div id="year1985"></div>
+        <div id="year1989"></div>
+        <div id="year1993"></div>
+        <div id="year1996"></div>
+        <div id="year1998"></div>
+        <div id="year1999"></div>
+        <div id="year2000a"></div>
+        <div id="year2000b"></div>
+    </div>
+</div>
+<div class="infoDate">
+    <p class="hide">Mari Carmen Alva <br>El camino hacia irma</p>
+    <div id="Infoyear1985" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1985: Voluntariados juveniles que empujan a la acción y descubren una pasión: ayudar me ayuda a crecer.
+        <?php else : ?>
+            1985: Youth volunteers that push into action and discover a passion: help helps me grow.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear1989" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1989: Un trabajo: Orientar a mujeres con embarazos en crisis. Una OSC que hace plantearme preguntas ¿por qué duele el aborto? ¿qué hay detrás de reincidir? ¿cómo sanar?
+        <?php else : ?>
+            1989: A work: Orienting women with pregnancies in crisis. A OSC that it makes me ask myself questions: why does abortion hurt? What is behind reoffending? How to heal?
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear1993" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1993: Una tesis de pedagogía; un buen pretexto para soñar. Un lugar para ayudar a quien ha pasado por el aborto. <br><br>Un sueño para que alguien más lo realice. Diseñar en papel, no hay nada más inofensivo “no obliga, ni compromete” alguien más podrá hacerlo.
+        <?php else : ?>
+            1993: A thesis of pedagogy; a good excuse to dream. A place to help those who have had an abortion. <br> <br> A dream for someone else to realize it. Design on paper, there is nothing more harmless "does not obligate, nor compromise" someone else can do it.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear1996" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1996: Del papel al examen profesional: los sinodales e invitados, fueron los primeros testigos e impulsores de que escribir si te compromete.
+        <?php else : ?>
+            1996: From the paper to the professional examination: the synods and guests were the first witnesses and impellers of what to write if you commit yourself.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear1998" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1998: Publicar un libro para compartir lo aprendido ¡No puede quedarse la tesis en una biblioteca universitaria! <br><br>Un proceso editorial que se detiene, al perder a mi bebé de 11 semanas de gestación ¡cómo confirmar que lo escrito, también lo siento en lo más hondo de mi ser! <br><br>La noticia de que un tercer hijo viene en camino, después de que sus hermanos mayores se llevan tan poquito tiempo uno del otro y con una economía familiar que asusta, me lleva a un medio compartido por muchas mujeres ¿podré? ¿es mayor mi susto que mi gusto? <br><br>Lo que conocía por referencia de algunas y por lectura de varios estudios, lo confirmaba: Un hijo que muere sin nacer te lleva a un dolor único.
+        <?php else : ?>
+            1998: Publish a book to share what has been learned The thesis can not remain in a university library! <br> <br> An editorial process that stops, when losing my baby of 11 weeks of gestation, how to confirm that what is written, I also feel it in the deepest part of my being! <br> <br> The news that a third child is on the way, after his older brothers they take so little time from each other and with a frightening family economy, takes me to a medium shared by many women. ? Is my shock greater than my taste? <br> <br> What I knew by reference of some and by reading several studies, confirmed it: A child who dies without being born takes you to a unique pain.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear1999" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            1999: “Nadie tiene derecho de abrir una herida si no está dispuesto a cerrarla” Si no sabe cómo hacerlo. <br><br>Y qué hacer si te invitan a un programa de radio en vivo, para hablar del libro “Y después del aborto ¿qué?” Y escuchas una y otra historia que se repiten en un “es la primera vez que hablo de mi aborto” “pensé que hablaban de mí al decir los síntomas que tengo” <br><br>Más allá de la empatía, fue tocar el dolor propio e imaginar cuantas veces estaría duplicado en cada mujer que no contaba con redes de apoyo, con oídos que la escucharán, ni con personas que las acompañaran, reconociendo o validando su pensar, su sentir.
+        <?php else : ?>
+            1999: "Nobody has the right to open a wound if it is not available to close it" If you do not know how to do it. <br> <br> What to do if I invite you to a live radio program, to talk about the book "And after the abortion What?". I speak of my abortion "" I thought they talked about me in the sense of saying the symptoms I have "<br> <br> Beyond empathy, it was to touch one's own pain and imagine how many times it is duplicated in each woman who does not He had support networks, with ears that will listen to him, with people who accompany, recognizing or validating his think, his feel.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear2000a" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            2000: Crear un espacio en donde expresar el pensar y sentir alrededor de un aborto, para entender lo sucedido y sanar. Sin ánimo de juicio, en donde hacer bien el BIEN inició por ser uno de los motores rodeando a IRMA de personas amigas y competentes.
+        <?php else : ?>
+            2000: Create a space in which to express the thinking and feeling around an abortion, to understand what happened and to heal. Without a sense of judgment, where to do well the GOOD started by being one of the engines surrounding IRMA of friendly and competent people.
+        <?php endif; ?></p>
+    </div>
+    <div id="Infoyear2000b" class="hide">
+        <p><?php if( 'es' == $lang ) : ?>
+            2000: Presente: IRMA, brinda atención psicológica especializada a quien ha perdido un bebé antes de nacer. <br><br>Siendo un espacio confidencial, profesional y libre de juicio.
+        <?php else : ?>
+            2000: Present: IRMA, provides specialized psychological care to those who have lost a baby before birth. <br><br> Being a confidential, professional and free of judgment space.
+        <?php endif; ?></p> 
+    </div>
+</div>
---
+++ themes/irma/templates/pages-include/quienes-somos/informes-anuales.php
@@ -0,0 +1,25 @@
+<?php $lang = isset( $_GET['lang'] ) ? $_GET['lang'] : 'es'; /* Iniciar WPML */ ?>
+<div id="gi_articulos" class="margin-top-large">    
+    <div class="margin-bottom-xlarge">  
+        <?php 
+            $args = array(
+                'post_type'         => 'gi_informe_anual',
+                'posts_per_page'    => -1,
+                'orderby'           => 'date',
+                'order'             => 'DESC'
+            ); 
+            $loop = new WP_Query( $args );
+            if ( $loop->have_posts() ) {
+                $i = 1;
+                while ( $loop->have_posts() ) : $loop->the_post(); 
+                    $custom_fields  = get_post_custom();
+                    $post_id        = get_the_ID();
+                    $url       = get_post_meta( $post_id, 'gi_informe_anual_url', true ); ?>
+
+                    <a href="<?php echo $url; ?>" class="color-primary block margin-bottom-small" target="_blank" title="Enlace a Informe anual <?php the_title(); ?>"><h3 class="fontfam-text margin-bottom-xsmall"><em class="icon-doc-text"></em> <?php if( 'es' == $lang ) : ?>Informe<?php else: ?>Report<?php endif; ?> <?php the_title(); ?></h3></a>
+
+                <?php $i++; endwhile;
+            } 
+            wp_reset_postdata(); ?>
+    </div>  
+</div>
